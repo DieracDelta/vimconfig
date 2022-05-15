6df1038d795df1f309efc21df91593ca31b1e048
Justin Restivo <justin@restivo.me>
2022-05-15 13:29:31 -0400

    feat: absolutely OP textobjects

--- modules/treesitter.nix
+++ modules/treesitter.nix
@@ -58,7 +58,7 @@ with dsl; {
         node_decremental = "<C-p>";
       };
     };
-    indent.enable = true;
+    # indent.enable = true;
     rainbow = {
       enable = true;
       disable = [ "html" ];
@@ -80,10 +80,105 @@ with dsl; {
         lookahead = true;
 
         keymaps = {
-          "['af']" = "@function.outer";
-          "['if']" = "@function.inner";
-          "['ac']" = "@class.outer";
-          "['ic']" = "@class.inner";
+          # functions
+          "['tfo']" = "@function.outer";
+          "['tfi']" = "@function.inner";
+          # classes
+          "['tco']" = "@class.outer";
+          "['tci']" = "@class.inner";
+          # boolean
+          "['too']" = "@conditional.outer";
+          "['toi']" = "@conditional.inner";
+          # comment "useless"
+          "['tuo']" = "@comment.outer";
+          "['tui']" = "@comment.inner";
+          # param
+          "['tpo']" = "@param.outer";
+          "['tpi']" = "@param.inner";
+          # loop
+          "['tlo']" = "@loop.outer";
+          "['tli']" = "@loop.inner";
+          # block
+          "['tbo']" = "@block.outer";
+          "['tbi']" = "@block.inner";
+          # statement (expression, bad use of e, agreed)
+          "['teo']" = "@statement.outer";
+          "['tei']" = "@statement.inner";
+        };
+      };
+      move = {
+        enable = true;
+        set_jumps = true; # whether to set jumps in the jumplist
+        goto_next_start = {
+          "['tmf']" = "@function.outer";
+          "['tmc']" = "@class.outer";
+          "['tmo']" = "@conditional.outer";
+          "['tmu']" = "@comment.outer";
+          "['tmp']" = "@param.outer";
+          "['tml']" = "@loop.outer";
+          "['tmb']" = "@block.outer";
+          "['tms']" = "@statement.outer";
+        };
+        goto_next_end = {
+          "['tmF']" = "@function.outer";
+          "['tmC']" = "@class.outer";
+          "['tmO']" = "@conditional.outer";
+          "['tmU']" = "@comment.outer";
+          "['tmP']" = "@param.outer";
+          "['tmL']" = "@loop.outer";
+          "['tmB']" = "@block.outer";
+          "['tmS']" = "@statement.outer";
+        };
+        goto_previous_start = {
+          "['tMf']" = "@function.outer";
+          "['tMc']" = "@class.outer";
+          "['tMo']" = "@conditional.outer";
+          "['tMu']" = "@comment.outer";
+          "['tMp']" = "@param.outer";
+          "['tMl']" = "@loop.outer";
+          "['tMb']" = "@block.outer";
+          "['tMs']" = "@statement.outer";
+        };
+        goto_previous_end = {
+          "['tMF']" = "@function.outer";
+          "['tMC']" = "@class.outer";
+          "['tMO']" = "@conditional.outer";
+          "['tMU']" = "@comment.outer";
+          "['tMP']" = "@param.outer";
+          "['tML']" = "@loop.outer";
+          "['tMB']" = "@block.outer";
+          "['tMS']" = "@statement.outer";
+        };
+      };
+      swap = {
+        enable = true;
+        swap_next = {
+          "['tsf']" = "@function.outer";
+          "['tsc']" = "@class.outer";
+          "['tso']" = "@conditional.outer";
+          "['tsu']" = "@comment.outer";
+          "['tsp']" = "@param.outer";
+          "['tsl']" = "@loop.outer";
+          "['tsb']" = "@block.outer";
+          "['tss']" = "@statement.outer";
+        };
+        swap_previous = {
+          "['tSf']" = "@function.outer";
+          "['tSc']" = "@class.outer";
+          "['tSo']" = "@conditional.outer";
+          "['tSu']" = "@comment.outer";
+          "['tSp']" = "@param.outer";
+          "['tSl']" = "@loop.outer";
+          "['tSb']" = "@block.outer";
+          "['tSs']" = "@statement.outer";
+        };
+      };
+      lsp_interop = {
+        enable = true;
+        border = "none";
+        peek_definition_code = {
+          "['<leader>df']" = "@function.outer";
+          "['<leader>dF']" = "@class.outer";
         };
       };
     };
