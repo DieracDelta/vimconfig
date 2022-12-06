8955f9741991ce6e30ffd6867a666ddc738515a8
Justin Restivo <justin@restivo.me>
2022-12-06 09:24:05 -0500

    feat: less abrasive column and rows

--- modules/essentials.nix
+++ modules/essentials.nix
@@ -207,8 +207,8 @@ in
       \   exe "normal! g`\"" |
       \ endif
 
-    autocmd BufRead,BufNewFile *  highlight CursorLine guibg=#6272A4
-    autocmd BufRead,BufNewFile * highlight CursorColumn guibg=#6272A4
+    autocmd BufRead,BufNewFile *  highlight CursorLine guibg=#63511f
+    autocmd BufRead,BufNewFile * highlight CursorColumn guibg=#63511f
 
   '';
 }
