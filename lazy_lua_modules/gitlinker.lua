4553214c7a5e2026fe24d6505f18cfe51ee26267
Justin Restivo <justin@restivo.me>
2026-01-04 10:54:04 -0500

    fix: git linker lazy loading

--- lazy_lua_modules/gitlinker.lua
+++ lazy_lua_modules/gitlinker.lua
@@ -1,11 +1,15 @@
-local lze = require("lze")
-
-lze.load({
-  "gitlinker.nvim",
+require("lze").load({
+  "gitlinker-nvim",
+  on_require = { "gitlinker" },
   keys = {
     { "<leader>gy", desc = "Git Linker" },
   },
+  load = function(name)
+    vim.cmd("packadd " .. name)
+  end,
   after = function()
     require("gitlinker").setup()
+    vim.api.nvim_set_keymap("n", "<leader>gy", "<cmd>GitLink<cr>", { desc = "Yank git link" })
+    vim.api.nvim_set_keymap("v", "<leader>gy", "<cmd>GitLink<cr>", { desc = "Yank git link" })
   end,
 })
